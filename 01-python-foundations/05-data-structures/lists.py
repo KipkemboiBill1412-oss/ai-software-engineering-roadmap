184c819eaade70e5f5221bd1921d35ceb4824a91
Bill Kipkemboi <your-email@example.com>
2026-08-11 03:02:57 +1000

    Add Python list exercises

---
+++ 01-python-foundations/05-data-structures/lists.py
@@ -0,0 +1,10 @@
+marks = [75, 80, 70, 85, 90]
+
+print(marks)
+##########
+marks = [75, 80, 70, 85, 90]
+
+print("All marks:", marks)
+print("First mark:", marks[0])
+print("Third mark:", marks[2])
+print("Last mark:", marks[4])
---
+++ 01-python-foundations/05-data-structures/student_marks.py
@@ -0,0 +1,9 @@
+marks = [45, 87, 86, 98, 45]
+
+print("All marks:", marks)
+print("First mark:", marks[0])
+print("Last mark:", marks[-1])
+
+print("Individual marks:")
+for mark in marks:
+    print(mark)
