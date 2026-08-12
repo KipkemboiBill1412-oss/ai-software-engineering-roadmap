09c9bc0d02a61e98893430a47f8ceedf51fd22f0
Bill Kipkemboi <your-email@example.com>
2026-08-12 22:39:22 +1000

    Add Python lists list_operations

---
+++ 01-python-foundations/05-data-structures/list_operations.py
@@ -0,0 +1,8 @@
+languages=["Python", "Javascript", "Java"]
+languages.append("C++")
+languages.pop(1)
+languages[1]= "C#"
+print("Final list: ", languages)
+print("Number of languages: ", len(languages))
+
+
