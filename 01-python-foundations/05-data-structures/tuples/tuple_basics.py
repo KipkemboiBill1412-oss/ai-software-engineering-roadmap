12975298004d6d6ab5655d7e63146428d8e01a7b
Bill Kipkemboi <your-email@example.com>
2026-08-18 20:24:56 +1000

    Add tuple basics and unpacking

---
+++ 01-python-foundations/05-data-structures/tuples/tuple_basics.py
@@ -0,0 +1,13 @@
+student=("Bill", 17, "Python")
+name, age, language = student
+print("Unpacked name: ", name)
+print("Unpacked age: ", age)
+print("Unpacked language: ", language)
+print("Number of items: ", len(student))
+if "Python" in student:
+    print("Python available: True")
+else:
+    print("Python available: False")
+
+
+
